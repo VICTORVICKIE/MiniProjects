c3bd51f9e6c0e2e9ce5c83f6887e5512cb0496d2
vignesh kumar <45727291+VICTORVICKIE@users.noreply.github.com>
2020-05-14 09:26:46 +0530

    Update streamfunc.py

--- streamfunc.py
+++ streamfunc.py
@@ -2,35 +2,34 @@ import matplotlib.pyplot as plt
 import sympy
 from sympy.abc import x, y
 import numpy as np
-import inspect
 
 
-def cylinder_stream_function(U=1, R=1):
-    r = sympy.sqrt(x**2 + y**2)
+def cylinder_stream_function(U=1, R=1):                             #Stream Function
+    r = sympy.sqrt(x**2 + y**2)                                     #converts x,y in terms of r,θ 
     theta = sympy.atan2(y, x)
     si = U * (r - R**2 / r) * sympy.sin(theta)
     #print(si)
     return si
 
-def velocity_field(psi):
-    u = sympy.lambdify((x, y), psi.diff(y), 'numpy')
-    v = sympy.lambdify((x, y), -psi.diff(x), 'numpy')
+def velocity_field(psi):                                            #Velocity Field
+    u = sympy.lambdify((x, y), psi.diff(y), 'numpy')                #Differentiates psi w.r.t.y for u and converts it to numpy function for calculation 
+    v = sympy.lambdify((x, y), -psi.diff(x), 'numpy')                #Differentiates psi w.r.t.x for -v and converts it to numpy function for calculation
     
     return u, v
 
-def plot_streamlines(ax, u, v, xlim=(-1, 1), ylim=(-1, 1)):
+def plot_streamlines(ax, u, v, xlim=(-1, 1), ylim=(-1, 1)):         #Plots streamlines and stagnation points
     x0, x1 = xlim
     y0, y1 = ylim
     Y, X =  np.ogrid[y0:y1:100j, x0:x1:100j]
     U=(psi.diff(y))
     V= -(psi.diff(x))
-    stag = sympy.solve([U,V],[x,y])
+    stag = sympy.solve([U,V],[x,y])                                 #solves u,v and returns x,y for u=v=0 stagnation pt
     for xp,yp in stag:
-        try:
-            plt.scatter(xp,yp,color='red',marker='o',)
-        except TypeError:
+        try:                                                        #Try block as the solution of stagnation pt contains imginary values
+            plt.scatter(xp,yp,color='red',marker='o',)              #plots real values and if imaginary values raises TypeError
+        except TypeError:                                           #this block catches the error 
             continue
-    ax.streamplot(X, Y, u(X, Y), v(X, Y), color='cornflowerblue')
+    ax.streamplot(X, Y, u(X, Y), v(X, Y), color='cornflowerblue')   #plots stream lines 
    
     
 
@@ -48,10 +47,7 @@ u, v = velocity_field(psi)
 xlim = ylim = (-3, 3)
 fig, ax = plt.subplots(figsize=(4, 4))
 plot_streamlines(ax, u, v, xlim, ylim)
-c = plt.Circle((0, 0), radius=1, facecolor='none', fill=False)
+c = plt.Circle((0, 0), radius=1, facecolor='none', fill=False)      #creates a circular 2D cylinder for better visualisation 
 ax.add_patch(c)
-#stagPoint = sympy.solve((u,v),[x,y])
-#for xp, yp in stagPoint:
- #   print(xp,yp)
 plt.show()
 format_axes(ax)
